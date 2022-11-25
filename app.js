0ce700977c95e5ed413ad326584444b735eab5f2
Semih Cihan <semih@lagotgames.com>
2022-11-25 14:43:43 +0300

    separate server and app

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-const createError = require('http-errors');
 const express = require('express');
+const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+// const hpp = require('hpp');
+const cors = require('cors');
 
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.enable('trust proxy');
+
+app.use(cors());
+app.use(helmet());
+app.options('*', cors());
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+}
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
+app.use(mongoSanitize());
+
 app.use('/users', usersRouter);
+app.use(xss());
+
+/* app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+); */
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -29,8 +64,5 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.send('heyaa');
 });
-
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {});
 
 module.exports = app;
